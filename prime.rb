ad99533dc4dbde830059df27e643220c3cc261f6
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-18 23:46:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  if int <= 1 
+    return false
+  elsif int == 2 || int == 3
+    return true 
+  elsif int > 3
+    nums = (2..int-1).to_a 
+    nums.each do |number|
+      if int % number == 0 
+        return false 
+      end
+    end
+    true
+  end
+end
